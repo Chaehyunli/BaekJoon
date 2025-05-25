f0960ffbe35b9f4ea8f4a68c194a66ceb7cd048f
Chaehyunlim <162084419+Chaehyunli@users.noreply.github.com>
2025-05-25 14:41:17 +0900

    [Bronze V] Title: 크냐?, Time: 188 ms, Memory: 17984 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            if(n == 0 && m == 0){
+                break;
+            }
+
+            if(n > m){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+
+        }
+
+        sc.close();
+
+    }
+}
