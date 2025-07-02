d6740dcd51c56abf6f7566b57d770894fd78475f
Chaehyunlim <162084419+Chaehyunli@users.noreply.github.com>
2025-07-02 10:48:46 +0900

    [Silver III] Title: Four Squares, Time: 204 ms, Memory: 18176 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;  // 최악의 경우 (1^2 + 1^2 + ... + 1^2)
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+        sc.close();
+    }
+}
